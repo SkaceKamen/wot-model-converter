e2c631b6bccbd8237b181a197357eb47d6abbd24
Jan Zípek <jan@zipek.cz>
2015-04-11 23:01:01 +0200

    Cleanup again

--- test.py
+++
@@ -1,7 +0,0 @@
-import wot
-
-reader = wot.ModelReader()
-
-with open("bld401_chouse.primitives", "rb") as prim:
-	with open("bld401_chouse.visual", "rb") as vis:
-		print(reader.read(prim, vis).getObj())
