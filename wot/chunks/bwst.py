74794ac2cee70821854689b1984b79af932f5a0e
Jan Zípek <jan@zipek.cz>
2016-02-06 12:17:40 +0100

    Fixed compatibility

--- wot/chunks/bwst.py
+++ wot/chunks/bwst.py
@@ -13,7 +13,7 @@ def get(f, debug=False):
 	for item in table["entries"]:
 		key = unp("<I", item[0:4])
 		f.seek(strings_start + unp("<I", item[4:8]))
-		strings[key] = f.read(unp("<I", item[8:12]))
+		strings[key] = f.read(unp("<I", item[8:12])).decode("UTF-8")
 		
 		if debug:
 			print (hex2(key, 8), strings[key])
